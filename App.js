d8074918061a00fd72b778d3e56bb6c4e0fed527
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-08-31 13:03:49 -0500

    import color constants  options to navi config add

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import ManageGroceryList from './screens/ManageGroceryList';
 import RecentGroceries from './screens/RecentGroceries';
 import AllGroceryLists from './screens/AllGroceryLists';
+import { GlobalStyles } from './constants/styles';
 
 // creating stack const holds results of calling stackNavi
 // stack const will hold object access to two comps
@@ -15,9 +17,36 @@ const BottomTabs = createBottomTabNavigator();
 // creating func comp for bottomtabs navi - nested navigation
 function GroceriesOverview() {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name="RecentGroceries" component={RecentGroceries} />
-      <BottomTabs.Screen name="AllGroceryLists" component={AllGroceryLists} />
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        headerTintColor: 'white',
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+      }}
+    >
+      <BottomTabs.Screen
+        name="RecentGroceries"
+        component={RecentGroceries}
+        options={{
+          title: 'Recent Groceries',
+          tabBarLabel: 'Recent',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="hourglass" size={size} color={color} />
+          ),
+        }}
+      />
+      <BottomTabs.Screen
+        name="AllGroceryLists"
+        component={AllGroceryLists}
+        options={{
+          title: 'All Grocery Lists',
+          tabBarLabel: 'All Lists',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar" size={size} color={color} />
+          ),
+        }}
+      />
     </BottomTabs.Navigator>
   );
 }
@@ -32,6 +61,9 @@ export default function App() {
           <Stack.Screen
             name="GroceriesOverview"
             component={GroceriesOverview}
+            options={{
+              headerShown: false,
+            }}
           />
           <Stack.Screen name="ManageGroceries" component={ManageGroceryList} />
         </Stack.Navigator>
---
+++ constants/styles.js
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: '##D5F9FF',
+    primary100: '#8FF9E1',
+    primary200: '##8FE3E1',
+    primary400: '#8FE3CF',
+    primary500: '#256D85',
+    primary700: '#2B4865',
+    primary800: '#002B5B',
+    accent500: '#f7bc0c',
+    error50: '#fcc4e4',
+    error500: '#9b095c',
+    gray500: '#39324a',
+    gray700: '#221c30',
+  },
+};
